979bf65fcb44f26164623e796a0db1e5724b72b5
zhipenglin <sunandmoon001@163.com>
2024-07-16 10:48:44 +0800

    添加说明

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const fp = require('fastify-plugin');
 const autoload = require('@fastify/autoload');
 const path = require('path');
 
+/**
+ * messageType:
+ *   发送渠道: 短信，邮箱
+ *   发送模板:
+ * channel: xxxx
+ * props:
+ *  模板变量的值
+ * */
+
 module.exports = fp(
   async (fastify, options) => {
     fastify.register(autoload, {
